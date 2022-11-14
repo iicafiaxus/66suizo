0664e1367e4f10cb78fd2622de09edd9f3bc196a
iicafiaxus <iica@live.jp>
2022-11-15 01:33:48 +0900

    Refactor: move pieces.

--- models/model.js
+++ models/model.js
@@ -13,32 +13,7 @@ model.cells = (function(model){
 	return cells;
 })(model);
 
-model.pieces = [
-	{id: 0, entity: pieceModel.entities.king, position: { x: 0, y: 3, face: 0, player: 1, isOut: false }},
-	{id: 1, entity: pieceModel.entities.king, position: { x: 5, y: 2, face: 0, player: 0, isOut: false }},
-	{id: 2, entity: pieceModel.entities.queen, position: { x: 0, y: 2, face: 0, player: 1, isOut: false }},
-	{id: 3, entity: pieceModel.entities.queen, position: { x: 5, y: 3, face: 0, player: 0, isOut: false }},
-	{id: 4, entity: pieceModel.entities.rook, position: { x: 0, y: 1, face: 0, player: 1, isOut: false }},
-	{id: 5, entity: pieceModel.entities.rook, position: { x: 5, y: 4, face: 0, player: 0, isOut: false }},
-	{id: 6, entity: pieceModel.entities.bishop, position: { x: 0, y: 4, face: 0, player: 1, isOut: false }},
-	{id: 7, entity: pieceModel.entities.bishop, position: { x: 5, y: 1, face: 0, player: 0, isOut: false }},
-	{id: 8, entity: pieceModel.entities.silver, position: { x: 0, y: 0, face: 0, player: 1, isOut: false }},
-	{id: 9, entity: pieceModel.entities.silver, position: { x: 0, y: 5, face: 0, player: 1, isOut: false }},
-	{id: 10, entity: pieceModel.entities.silver, position: { x: 5, y: 0, face: 0, player: 0, isOut: false }},
-	{id: 11, entity: pieceModel.entities.silver, position: { x: 5, y: 5, face: 0, player: 0, isOut: false }},
-	{id: 12, entity: pieceModel.entities.pawn, position: { x: 1, y: 0, face: 0, player: 1, isOut: false }},
-	{id: 13, entity: pieceModel.entities.pawn, position: { x: 1, y: 1, face: 0, player: 1, isOut: false }},
-	{id: 14, entity: pieceModel.entities.pawn, position: { x: 1, y: 2, face: 0, player: 1, isOut: false }},
-	{id: 15, entity: pieceModel.entities.pawn, position: { x: 1, y: 3, face: 0, player: 1, isOut: false }},
-	{id: 16, entity: pieceModel.entities.pawn, position: { x: 1, y: 4, face: 0, player: 1, isOut: false }},
-	{id: 17, entity: pieceModel.entities.pawn, position: { x: 1, y: 5, face: 0, player: 1, isOut: false }},
-	{id: 18, entity: pieceModel.entities.pawn, position: { x: 4, y: 0, face: 0, player: 0, isOut: false }},
-	{id: 19, entity: pieceModel.entities.pawn, position: { x: 4, y: 1, face: 0, player: 0, isOut: false }},
-	{id: 20, entity: pieceModel.entities.pawn, position: { x: 4, y: 2, face: 0, player: 0, isOut: false }},
-	{id: 21, entity: pieceModel.entities.pawn, position: { x: 4, y: 3, face: 0, player: 0, isOut: false }},
-	{id: 22, entity: pieceModel.entities.pawn, position: { x: 4, y: 4, face: 0, player: 0, isOut: false }},
-	{id: 23, entity: pieceModel.entities.pawn, position: { x: 4, y: 5, face: 0, player: 0, isOut: false }},
-];
+model.pieces = pieceModel.pieces;
 
 model.checkCanMove = (piece, cell, positions) => {
 	if(positions.find(pos =>
