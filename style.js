0df3cc48d48ab90ce575696c1f1f197dab21d5d2
iicafiaxus <iica@live.jp>
2023-04-03 00:44:04 +0900

    Add wide window layout.

--- style.js
+++ style.js
@@ -2,10 +2,13 @@ void function(){
 	const onResize = function(ev){
 		const width = document.documentElement.clientWidth;
 		const height = document.documentElement.clientHeight;
-		const magnitude = Math.max(
-			Math.min(width / 980, height / 1760)
-		);
-		const widthOffset = (width / magnitude - 980) / 2;
+		const magnitudeNormal = Math.min(width / 980, height / 1760);
+		const magnitudeWide = Math.min(width / 1548, height / 1106);
+		const magnitude = Math.max(magnitudeNormal, magnitudeWide);
+		const widthOriginal = (magnitude == magnitudeNormal) ? 980 : 1548;
+		const widthOffset = (width / magnitude - widthOriginal) / 2;
+		document.body.classList[(magnitude == magnitudeWide) ? "add" : "remove"]("wide");
+
 		if( ! document.getElementById("style-all-magnitude")){
 			const style = document.createElement("style");
 			style.id = "style-all-magnitude";
